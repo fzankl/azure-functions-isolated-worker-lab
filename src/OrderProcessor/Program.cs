8e162a5a71790ac83943076dd2f8c28d9bafc469
fzankl <fabian@fzankl.de>
2026-08-28 22:04:49 +0200

    Serialization attributes: Newtonsoft through AddNewtonsoftJson

--- src/OrderProcessor/Program.cs
+++ src/OrderProcessor/Program.cs
@@ -1,8 +1,14 @@
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
+
+// Variante B: auf Newtonsoft bleiben, aber in der Schicht, die die Antwort
+// tatsächlich schreibt. Das ist die MVC-Formatierschicht von ASP.NET Core,
+// nicht WorkerOptions.Serializer. Siehe docs/serializer-attributes.md.
+builder.Services.AddControllers().AddNewtonsoftJson();
 
 builder.Build().Run();
